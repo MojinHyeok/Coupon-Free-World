e7e8aa204a91378947965744381075250ce67896
이요면[광주_1반_C101]팀원 <yyiz1135@naver.com>
2021-07-19 11:25:27 +0900

    add to java file

---
+++ backend/src/main/java/com/ssafy/backend/board/model/BoardModel.java
@@ -0,0 +1,47 @@
+package com.ssafy.backend.board.model;
+
+public class BoardModel {
+	int boardNum;
+	int noticeOption;
+	String title;
+	String contents;
+	String writer;
+	String regDate;
+	
+	public int getBoardNum() {
+		return boardNum;
+	}
+	public void setBoardNum(int boardNum) {
+		this.boardNum = boardNum;
+	}
+	public int getNoticeOption() {
+		return noticeOption;
+	}
+	public void setNoticeOption(int noticeOption) {
+		this.noticeOption = noticeOption;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+}
