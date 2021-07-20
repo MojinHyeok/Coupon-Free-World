a23314a3b9908b28e370bf46b9abcf0a5561b395
Chanwoo Min <cksdn135@kookmin.ac.kr>
2021-07-21 01:55:54 +0900

    머지충돌 수정1

--- frontend/src/components/common/AppHeader.vue
+++ frontend/src/components/common/AppHeader.vue
@@ -1,6 +1,5 @@
 <template>
   <header>
-<<<<<<< frontend/src/components/common/AppHeader.vue
     <router-link to="/board/list">List</router-link> |
     <router-link to="/account/signup">signup</router-link> |
     <router-link to="/account/login">login</router-link> |
