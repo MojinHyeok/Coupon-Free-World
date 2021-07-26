dae8c8f93f644b2d7a4af64339da685b8fb275ec
이요면[광주_1반_C101]팀원 <yyiz1135@naver.com>
2021-07-26 11:21:18 +0900

    update comment

--- backend/src/main/java/com/ssafy/backend/comment/mapper/CommentMapper.java
+++ backend/src/main/java/com/ssafy/backend/comment/mapper/CommentMapper.java
@@ -1,5 +1,11 @@
 package com.ssafy.backend.comment.mapper;
 
-public interface CommentMapper {
+import org.springframework.stereotype.Repository;
 
+import com.ssafy.backend.comment.model.CommentModel;
+
+@Repository
+public interface CommentMapper {
+	CommentModel boardComment(CommentModel model) throws Exception;
+	CommentModel feedComment(CommentModel model) throws Exception;
 }
--- backend/src/main/java/com/ssafy/backend/comment/service/CommentService.java
+++ backend/src/main/java/com/ssafy/backend/comment/service/CommentService.java
@@ -1,5 +1,12 @@
 package com.ssafy.backend.comment.service;
 
-public interface CommentService {
+import com.ssafy.backend.comment.model.CommentModel;
 
+public interface CommentService {
+	// 게시판에 코멘트 달기
+	public CommentModel boardComment(CommentModel model) throws Exception;
+	
+	// 상대방 피드에 코멘트 달기
+	public CommentModel feedComment(CommentModel model) throws Exception;
+	
 }
