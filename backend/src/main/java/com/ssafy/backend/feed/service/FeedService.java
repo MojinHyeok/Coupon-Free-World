7e4a3c943ef69216bea9c4f596c70491688a2949
이요면[광주_1반_C101]팀원 <yyiz1135@naver.com>
2021-07-27 16:55:32 +0900

    update feed service

--- backend/src/main/java/com/ssafy/backend/feed/service/FeedService.java
+++ backend/src/main/java/com/ssafy/backend/feed/service/FeedService.java
@@ -1,5 +1,19 @@
 package com.ssafy.backend.feed.service;
 
-public interface FeedService {
+import org.springframework.data.repository.query.Param;
 
+import com.ssafy.backend.feed.model.FeedModel;
+
+public interface FeedService {
+	// 피드 작성
+	int writeFeed(FeedModel model) throws Exception;
+	
+	// 피드 삭제
+	int deleteFeed(int feedID) throws Exception;
+	
+	// 좋아요 설정
+	int incLikeCnt(@Param("feedID") int feedID, @Param("likeCount") int likeCount) throws Exception;
+	
+	// 좋아요 해제
+	int decLikeCnt(@Param("feedID") int feedID, @Param("likeCount") int likeCount) throws Exception;
 }
--- backend/src/main/java/com/ssafy/backend/feed/service/FeedServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/feed/service/FeedServiceImpl.java
@@ -1,8 +1,34 @@
 package com.ssafy.backend.feed.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ssafy.backend.feed.mapper.FeedMapper;
+import com.ssafy.backend.feed.model.FeedModel;
 
 @Service
 public class FeedServiceImpl implements FeedService {
+	@Autowired
+	FeedMapper mapper;
+	
+	@Override
+	public int writeFeed(FeedModel model) throws Exception {
+		return mapper.writeFeed(model);
+	}
+
+	@Override
+	public int deleteFeed(int feedID) throws Exception {
+		return mapper.deleteFeed(feedID);
+	}
+
+	@Override
+	public int incLikeCnt(int feedID, int likeCount) throws Exception {
+		return mapper.incLikeCnt(feedID, likeCount);
+	}
+
+	@Override
+	public int decLikeCnt(int feedID, int likeCount) throws Exception {
+		return mapper.decLikeCnt(feedID, likeCount);
+	}
 
 }
