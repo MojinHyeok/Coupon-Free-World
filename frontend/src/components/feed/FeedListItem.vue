758f1f9a520f336f51a5d87430b43749b37258a9
Chanwoo Min <cksdn135@kookmin.ac.kr>
2021-08-02 16:19:26 +0900

    S05P13C101-44 / CRD 로직 구현&UserEditForm수정

---
+++ frontend/src/components/feed/FeedListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>userID: {{ feedItem.userID }}</div>
+    <div>feedID: {{ feedItem.feedID }}</div>
+    <div>내용: {{ feedItem.content }}</div>
+    <div>날짜: {{ feedItem.date }}</div>
+    <div>좋아요: {{ feedItem.likeCnt }}</div>
+    <div>사진1url: {{ feedItem.photoPath1 }}</div>
+    <div>사진2url: {{ feedItem.photoPath2 }}</div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    feedItem: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    // 해당 게시글 클릭시 상세페이지 이동
+    moveDetail() {
+      this.$router.push(`/feed/${this.feedItem.feedID}`)
+    },
+    // 유저 클릭시 유저Profile 이동
+    moveProfile() {},
+    // 좋아요 클릭시
+    clickLike() {},
+  },
+}
+</script>
+
+<style></style>
---
+++ frontend/src/views/feed/CreatePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Create페이지</h1>
+    <FeedCreateForm></FeedCreateForm>
+  </div>
+</template>
+
+<script>
+import FeedCreateForm from '@/components/feed/FeedCreateForm'
+export default {
+  components: {
+    FeedCreateForm,
+  },
+}
+</script>
+
+<style></style>
