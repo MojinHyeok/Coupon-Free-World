cb7dff78141213026393a6da43ad999f0e47e9fa
multicampus <blessisu@naver.com>
2021-08-06 11:58:55 +0900

    ppt update

---
+++ .history/frontend/src/components/board/SearchBar_20210804164749.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="search-bar">
+    <input type="text" @keyup.enter="onKeywordEnter" />
+    <button>
+      <i class="bi bi-search" style="height: 30;"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+import { searchBoard } from '@/api/boards'
+
+export default {
+  name: 'SearchBar',
+  methods: {
+    async onKeywordEnter(event) {
+      const keyword = event.target.value
+      const res = await searchBoard('title', keyword, 10, 0)
+      const boardListTotal = res.data
+      const boardTotal = boardListTotal.length
+      this.$emit('on-keyword-enter', boardListTotal, boardTotal)
+    },
+  },
+}
+</script>
