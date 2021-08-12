42eb2733496f85d0b8bab80348ba12faf9760b6e
모진혁 <gkgk246@naver.com>
2021-08-12 16:55:46 +0900

    commit

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
+  configureWebpack: {
+    // other webpack options to merge in ...
+  },
   devServer: {
     overlay: false,
+  },
+  // devServer Options don't belong into `configureWebpack`
+  devServer: {
+    host: '0.0.0.0',
+    hot: true,
+    disableHostCheck: true,
   },
 }
